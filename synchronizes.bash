d306dfe37e946d8ffca237a93370c958a65af422
Payne <wuzhipeng1289690157@gmail.com>
2022-01-08 17:00:27 +0800

    push or fix some commit

--- synchronizes.bash
+++ synchronizes.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-git pull gitee main
+#git pull gitee main
+git pull origin
 git add -A && git commit -am "push or fix some commit"
-git push gitee
+#git push gitee
 git push origin
