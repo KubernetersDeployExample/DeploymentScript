ebd9d9cb89e7feb65d9bd24a6e18831c705c9fe7
payne-wu <wuzhipeng1289690157@gmail.com>
2022-11-11 10:32:20 +0800

    Canonical Naming and Formatting

--- MiddleWare/Kafka/depoy.sh
+++ MiddleWare/Kafka/depoy.sh
@@ -2,6 +2,6 @@
 kubectl apply -f zookeeper.yaml && kubectl apply -f kafka-svc.yaml
 cat kafka.yaml | sed \
   "s/@kafka_service_clusterIp/$(kubectl get svc kafka-service -n middleware | grep \
-  NodePort | awk '{print $3}')/g" | sed \
+    NodePort | awk '{print $3}')/g" | sed \
   "s/@zookeeper_service_clusterIp/$(kubectl get svc zookeeper-service -n middleware | grep \
-  ClusterIP | awk '{print $3}')/g" | kubectl apply -f -
+    ClusterIP | awk '{print $3}')/g" | kubectl apply -f -
